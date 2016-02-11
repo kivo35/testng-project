a49574127cb43eef3d1d505de64eb1e95e613dc8
Kirill <kivo35@hotmail.com>
2016-02-10 19:10:33 -0800

    Create new helper class to display objects information

---
+++ src/main/java/com/sqa/kv/util/helper/Info.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package com.sqa.kv.util.helper;
+
+/**
+ * Create a helper class to add to your framework which gives information on any
+ * class you create. The method signatures are below. Try to implement one at a
+ * time. Create a test case for each method.
+ * 
+ * @author Kirill
+ * 
+ */
+public class Info
+{
+
+	public static void display2DInfo(Object[][] objects)
+	{
+
+	}
+
+	public static void displayInfo(Object obj)
+	{
+
+	}
+
+	public static void displayInfo(Object obj1, Object obj2, Object... objN)
+	{
+
+	}
+
+	public static void displayInfo(Object[] obj)
+	{
+
+	}
+
+	public static String get2DInfo(Object[][] objects)
+	{
+		// TODO show the objects in 2D dimension
+		return null;
+	}
+
+	public static String getInfo(Object obj)
+	{
+		return null;
+	}
+}
